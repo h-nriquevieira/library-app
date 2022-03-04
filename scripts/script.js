2440b2f571b22bb188a3168e99dde363d5bd80ae
h-nriquevieira <h.nriquevieira@gmail.com>
2022-03-04 13:39:32 -0300

    Rewrite the code from scratch
    I was getting lost on the project, so decided to start fresh and write
    cleaner code.

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,51 @@
-let myLibrary = [];
 const modal = document.querySelector('.modal');
 const addBookButton = document.querySelector('.add-book');
 const closeButton = document.querySelector('.close');
-const sendBookButton = document.querySelector('.send-book');
-const currentTitle = document.querySelector('#title');
-const currentAuthor = document.querySelector('#author');
-const currentPages = document.querySelector('#pages');
-const currentRead = document.querySelector('#read');
+
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
+}
+
+Book.prototype.info = function() {
     let isItRead = 'not read yet';
     if (this.read) {
       isItRead = 'already read'
     }
     return `${this.title} by ${this.author}, ${this.pages} pages, ${isItRead}.`;
+}
+
+let myLibrary = [];
+
+
+function addBookToLibrary(book) {
+  const bookCard = document.createElement('div');
+  const title = document.createElement('h2');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const read = document.createElement('p');
+  bookCard.classList.add('.book-card');
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+  read.textContent = book.read ? 'already read' : 'not read';
+  bookCard.append(title, author, pages, read);
+  myLibrary.push(bookCard);
+}
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    document.body.appendChild(myLibrary[i]);
   }
 }
 
-myLibrary.push(new Book('The Hobbit', 'Tolkien', 295, true));
-myLibrary.push(new Book('The Great Gatsby', 'Francis Scott Fitzgerald', 163, true));
+const hobbit = new Book('The Hobbit', 'JRR Tolkien', 295, true);
 
-function addBookToLibrary(title, author, pages, isItRead) {
-  myLibrary.push(new Book(title, author, pages, isItRead));
-}
 
-function addBook() {
-  displayBooks(currentTitle, currentAuthor, currentPages, currentRead);
-  resetInput();
-}
-
-function resetInput() {
-  currentTitle.value = '';
-  currentAuthor.value = '';
-  currentPages.value = '';
-  currentRead.checked = false;
-  modal.style.display = 'none';
-}
-
-function displayBooks(title, author, pages, read) {
-  let bookCard = document.createElement('div');
-  let bookTitle = document.createElement('h2');
-  let bookAuthor = document.createElement('p');
-  let bookPages = document.createElement('p');
-  let bookRead = document.createElement('p');
-  bookTitle.textContent = title.value;
-  bookAuthor.textContent = author.value;
-  bookPages.textContent = pages.value;
-  if (read.checked) {
-    bookRead.textContent = 'Read';
-  } else {bookRead.textContent = 'Not read'}
-  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
-  document.body.appendChild(bookCard);
-}
 
 addBookButton.addEventListener('click', () => modal.style.display = 'block');
 closeButton.addEventListener('click', () => modal.style.display = 'none');
-sendBookButton.addEventListener('click', addBook);
-window.addEventListener('click', (e) => {if(e.target == modal) modal.style.display = 'none'});
+window.addEventListener('click', e => { if(e.target == modal) {modal.style.display = 'none'}});
